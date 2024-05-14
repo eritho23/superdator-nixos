9da871a2d77ffc1b7db8410ff80d2343ee08bbb3
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2024-05-14 20:55:29 +0200

    apply formatting

--- flake.nix
+++ flake.nix
@@ -6,17 +6,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11-small";
   };
 
-  outputs = { self, nixpkgs, ...} @inputs: 
-  let
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
     inherit (self) outputs;
-  in
-  {
+  in {
     nixosConfigurations = {
       superdator = pkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
-        modules = [ ./nixos/superdator/configuration.nix ];
+        modules = [./nixos/superdator/configuration.nix];
       };
     };
   };
