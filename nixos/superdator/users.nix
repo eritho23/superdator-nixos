b40b26d3173ab1a17863c248bd8d94d16f65cbca
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2024-05-14 21:45:07 +0200

    comment out unused code

--- nixos/superdator/users.nix
+++ nixos/superdator/users.nix
@@ -4,19 +4,19 @@
     uid = 1009;
     initialPassword = "1234";
     extraGroups = ["wheel"];
-    packages = with pkgs; [bat fzf ripgrep helix];
+    # packages = with pkgs; [bat fzf ripgrep helix];
   };
 
-  users.users."temp-debian" = {
-    isNormalUser = true;
-    uid = 1010;
-    initialPassword = "debian";
-    packages = [
-      (pkgs.writeShellScriptBin "debian-container" ''
-        ${pkgs.podman}/bin/podman run --rm docker.io/library/debian:12 bash
+  # users.users."temp-debian" = {
+  #   isNormalUser = true;
+  #   uid = 1010;
+  #   initialPassword = "debian";
+  #   packages = [
+  #     (pkgs.writeShellScriptBin "debian-container" ''
+  #       ${pkgs.podman}/bin/podman run --rm docker.io/library/debian:12 bash
 
-        exit 0
-      '')
-    ];
-  };
+  #       exit 0
+  #     '')
+  #   ];
+  # };
 }
