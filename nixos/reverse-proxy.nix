f1112962b952cbcd866cf101bcd0c63f45a9d923
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2024-06-10 16:17:52 +0200

    add hello test config

--- nixos/reverse-proxy.nix
+++ nixos/reverse-proxy.nix
@@ -1,5 +1,13 @@
 {
   services.caddy = {
     enable = true;
+    email = "eric.thorburn@hitachigymnasiet.se";
+
+    virtualHosts = {
+      ":80,:443".extraConfig = ''
+        respond "Hello, World!"
+        tls internal
+      '';
+    };
   };
 }
