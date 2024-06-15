b810a9524eb07bb7670444c1370d5c0ccd2ec816
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2024-06-15 15:45:17 +0200

    disable reverse proxy

--- nixos/reverse-proxy.nix
+++ nixos/reverse-proxy.nix
@@ -1,13 +1,19 @@
-{
-  services.caddy = {
-    enable = true;
-    email = "eric.thorburn@hitachigymnasiet.se";
+# {config}:
+# {
+#   services.caddy = {
+#     enable = true;
+#     email = "eric.thorburn@hitachigymnasiet.se";
 
-    virtualHosts = {
-      ":80,:443".extraConfig = ''
-        respond "Hello, World!"
-        tls internal
-      '';
-    };
-  };
-}
+#     virtualHosts = {
+#       ":80, :443"= {
+#         extraConfig = ''
+#           respond "Hello, World!"
+#           tls internal
+#         '';
+#         logFormat = ''
+#           output file ${config.services.caddy.logDir}-access-https.log
+#         '';
+#       };
+#     };
+#   };
+# }
