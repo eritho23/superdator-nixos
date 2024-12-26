2318eb54a2da4502b6f41dbf4e77bb4dbb94675d
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2024-12-26 18:50:49 +0100

    try without -bin suffix

--- nixos/services.nix
+++ nixos/services.nix
@@ -27,43 +27,45 @@
     acceleration = "cuda";
   };
 
-  # services.jupyterhub = {
-    # enable = true;
-    # authentication = "jupyterhub.auth.PAMAuthenticator";
-    # kernels = {
-      # torch = let
-        # env = pkgs.python311.withPackages (pythonPackages:
-          # with pythonPackages; [
-            # # Base for the kernel
-            # ipykernel
-# 
-            # # Useful utilities
-            # beautifulsoup4 # Web scraping
-            # matplotlib # Graphs
-            # numpy # Of course
-            # pandas # CSV files
-            # pillow # Images
-            # requests # Make API requests
-            # scipy # Superset of numpy
-            # torch-bin # PyTorch
-            # torchaudio-bin
-            # torchvision-bin
-          # ]);
-      # in {
-        # displayName = "Machine learning kernel (PyTorch)";
-        # argv = [
-          # "${env.interpreter}"
-          # "-m"
-          # "ipykernel_launcher"
-          # "-f"
-          # "{connection_file}"
-        # ];
-        # language = "python";
-        # logo32 = "${env}/${env.sitePackages}/ipykernel/resources/logo-32x32.png";
-        # logo64 = "${env}/${env.sitePackages}/ipykernel/resources/logo-64x64.png";
-      # };
-    # };
-  # };
+  # TODO: fix jupyterhub
+
+  services.jupyterhub = {
+    enable = true;
+    authentication = "jupyterhub.auth.PAMAuthenticator";
+    kernels = {
+      torch = let
+        env = pkgs.python311.withPackages (pythonPackages:
+          with pythonPackages; [
+            # Base for the kernel
+            ipykernel
+
+            # Useful utilities
+            beautifulsoup4 # Web scraping
+            matplotlib # Graphs
+            numpy # Of course
+            pandas # CSV files
+            pillow # Images
+            requests # Make API requests
+            scipy # Superset of numpy
+            torch # PyTorch
+            torchaudio
+            torchvision
+          ]);
+      in {
+        displayName = "Machine learning kernel (PyTorch)";
+        argv = [
+          "${env.interpreter}"
+          "-m"
+          "ipykernel_launcher"
+          "-f"
+          "{connection_file}"
+        ];
+        language = "python";
+        logo32 = "${env}/${env.sitePackages}/ipykernel/resources/logo-32x32.png";
+        logo64 = "${env}/${env.sitePackages}/ipykernel/resources/logo-64x64.png";
+      };
+    };
+  };
 
   nixpkgs.config.cudaSupport = true;
 }
