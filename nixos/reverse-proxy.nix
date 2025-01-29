09e96f1ee26079b31c8c9cadb597fede4163195e
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-01-29 19:45:29 +0100

    add config for subdomain

--- nixos/reverse-proxy.nix
+++ nixos/reverse-proxy.nix
@@ -19,11 +19,12 @@
       };
       "chat.superdator.spetsen.net" = {
         extraConfig = ''
-                 forward_auth 127.0.0.1:9091 {
+	 forward_auth 127.0.0.1:9091 {
             uri /api/authz/forward-auth
             copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
-                 }
-          reverse_proxy 127.0.0.1:9999
+	 }
+         # reverse_proxy 127.0.0.1:9999
+	 respond "Disabled"
 
         '';
       };
@@ -41,6 +42,13 @@
         extraConfig = ''
           reverse_proxy 127.0.0.1:8091
         '';
+      };
+      "boka.spetsen.net" = {
+        extraConfig = ''
+	  respond "Coming soon"
+          # reverse_proxy @websockets 127.0.0.1:5001
+	  # reverse_proxy / 127.0.0.1:5000
+	'';
       };
     };
   };
