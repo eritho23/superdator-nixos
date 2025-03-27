3331bb3083b8cc44544ccd7db97cf05bb5abb58f
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-03-27 10:53:45 +0100

    changes

--- nixos/deployments.nix
+++ nixos/deployments.nix
@@ -40,38 +40,48 @@
   # };
   # };
 
-  # containers.justcount = {
-  # autoStart = true;
-  # config = {
-  # config,
-  # pkgs,
-  # ...
-  # }: let
-  # pbDataDir = "/var/lib/pocketbase";
-  # pbListenAddr = "127.0.0.1:8092";
-  # in {
-  # system.stateVersion = "23.11";
-  # environment.systemPackages = with pkgs; [pocketbase];
-  # users.users.pocketbase = {
-  # isSystemUser = true;
-  # home = pbDataDir;
-  # shell = "/run/current-system/sw/bin/nologin";
-  # group = "pocketbase";
-  # createHome = true;
-  # };
-  # users.groups.pocketbase = {};
-  # systemd.services.zmartrest-pb = {
-  # unitConfig.description = "Pocketbase for zmartrest";
-  # serviceConfig = {
-  # ExecStart = "${pkgs.pocketbase}/bin/pocketbase serve --dir ${pbDataDir}/pb_data --publicDir ${pbDataDir}/pb_public --hooksDir ${pbDataDir}/pb_hooks --http ${pbListenAddr}";
-  # Restart = "always";
-  # RestartSec = "5s";
-  # Type = "simple";
-  # User = "pocketbase";
-  # Group = "pocketbase";
-  # };
-  # wantedBy = ["multi-user.target"];
-  # };
-  # };
-  # };
+  containers.justcount = {
+    autoStart = true;
+    config = {
+      config,
+      pkgs,
+      ...
+    }: let
+      pbDataDir = "/var/lib/pocketbase";
+      pbListenAddr = "127.0.0.1:8092";
+      pbPackage = pkgs.pocketbase.overrideAttrs (f: rec {
+        version = "0.26.4";
+        src = pkgs.fetchFromGitHub {
+          owner = "pocketbase";
+          repo = "pocketbase";
+          rev = "v${version}";
+          hash = "sha256-rNosfeiuXsn2x8Ae7WIZLmF7njsgi8fJ1Ze1njYQSY0=";
+        };
+	vendorHash = pkgs.lib.fakeHash;
+      });
+    in {
+      system.stateVersion = "23.11";
+      environment.systemPackages = with pkgs; [pocketbase];
+      users.users.pocketbase = {
+        isSystemUser = true;
+        home = pbDataDir;
+        shell = "/run/current-system/sw/bin/nologin";
+        group = "pocketbase";
+        createHome = true;
+      };
+      users.groups.pocketbase = {};
+      systemd.services.justcount-pb = {
+        unitConfig.description = "Pocketbase for justcount";
+        serviceConfig = {
+          ExecStart = "${pbPackage}/bin/pocketbase serve --dir ${pbDataDir}/pb_data --publicDir ${pbDataDir}/pb_public --hooksDir ${pbDataDir}/pb_hooks --http ${pbListenAddr}";
+          Restart = "always";
+          RestartSec = "5s";
+          Type = "simple";
+          User = "pocketbase";
+          Group = "pocketbase";
+        };
+        wantedBy = ["multi-user.target"];
+      };
+    };
+  };
 }
