e8618d9bfbf622e6ae36923eed2699eec5216410
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-08-21 20:51:23 +0200

    add ssh access

--- nixos/remotebuild.nix
+++ nixos/remotebuild.nix
@@ -5,6 +5,7 @@
     isNormalUser = true;
     createHome = false;
     group = "remotebuild";
+    extraGroups = ["ssh-access"];
 
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFdfSfFNnug6V40jL0MDZzsEVQ+l8IIr3CNsjVXT39q root@SW2311"];
   };
