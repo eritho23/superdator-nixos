c77fca77f042f58bdc7569c9d5d80cb4ac79b3ca
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-09-25 08:27:58 +0200

    sdf

--- nixos/deployments.nix
+++ nixos/deployments.nix
@@ -51,51 +51,6 @@
     };
   };
 
-  # containers.fardtjanst = {
-  #   autoStart = true;
-  #   config = {
-  #     config,
-  #     pkgs,
-  #     ...
-  #   }: let
-  #     pbDataDir = "/var/lib/pocketbase";
-  #     pbListenAddr = "127.0.0.1:8093";
-  #     pbPackage = pkgs.pocketbase.overrideAttrs (f: rec {
-  #       version = "0.26.4";
-  #       src = pkgs.fetchFromGitHub {
-  #         owner = "pocketbase";
-  #         repo = "pocketbase";
-  #         rev = "v${version}";
-  #         hash = "sha256-rNosfeiuXsn2x8Ae7WIZLmF7njsgi8fJ1Ze1njYQSY0=";
-  #       };
-  #       vendorHash = "sha256-9TuKYXZyyfVaow/5SW0rbYlIs8XiNF1hmkmHZT9J2O0=";
-  #     });
-  #   in {
-  #     system.stateVersion = "23.11";
-  #     environment.systemPackages = with pkgs; [pocketbase];
-  #     users.users.pocketbase = {
-  #       isSystemUser = true;
-  #       home = pbDataDir;
-  #       shell = "/run/current-system/sw/bin/nologin";
-  #       group = "pocketbase";
-  #       createHome = true;
-  #     };
-  #     users.groups.pocketbase = {};
-  #     systemd.services.fardtjanst-pb = {
-  #       unitConfig.description = "Pocketbase for fardtjanst";
-  #       serviceConfig = {
-  #         ExecStart = "${pbPackage}/bin/pocketbase serve --dir ${pbDataDir}/pb_data --publicDir ${pbDataDir}/pb_public --hooksDir ${pbDataDir}/pb_hooks --http ${pbListenAddr}";
-  #         Restart = "always";
-  #         RestartSec = "5s";
-  #         Type = "simple";
-  #         User = "pocketbase";
-  #         Group = "pocketbase";
-  #       };
-  #       wantedBy = ["multi-user.target"];
-  #     };
-  #   };
-  # };
-
   users.groups.spetsctf = {};
   users.users.spetsctf = let
     spetsCtfDataDir = "/var/lib/spetsctf";
@@ -149,9 +104,9 @@
     serviceConfig = {
       DynamicUser = "yes";
       EnvironmentFile = config.sops.secrets."aulabokning/environment_file".path;
-      ExecStart = "${lib.getExe pkgs.nodejs_22} ${aulabokningPath}/server.js";
+      ExecStart = "${lib.getExe pkgs.nodejs_22} --jitless ${aulabokningPath}/server.js";
       Group = "redis-aulabokning";
-      # MemoryDenyWriteExecute = "yes"; # Consider if --jitless is desirable.
+      MemoryDenyWriteExecute = "yes";
       NoNewPrivileges = "yes";
       PrivateDevices = "yes";
       PrivateTmp = "yes";
