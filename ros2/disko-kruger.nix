5e6c62bcedce1744139df6b3782f44199909ff88
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-10-08 15:19:47 +0200

    showtime

--- ros2/disko-kruger.nix
+++ ros2/disko-kruger.nix
@@ -4,7 +4,7 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/disk/by-id/..."; # FIXME
+        device = "/dev/disk/by-id/nvme-WD_BLACK_SN7100_1TB_25342B802933";
         type = "disk";
         content = {
           type = "gpt";
