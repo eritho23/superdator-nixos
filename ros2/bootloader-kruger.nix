4c22f006646a25bfd9bc729088948ea1f2185c0c
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-10-08 15:58:28 +0200

    whoops 38

--- ros2/bootloader-kruger.nix
+++ ros2/bootloader-kruger.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-systemd-boot.enable = lib.mkForce false;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/var/lib/sbctl";
