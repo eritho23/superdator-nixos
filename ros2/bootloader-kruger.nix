aaa20337d615a152254c38881361b1cba6682777
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-10-08 15:20:28 +0200

    whoops 34

--- ros2/bootloader-kruger.nix
+++ ros2/bootloader-kruger.nix
@@ -3,7 +3,7 @@
   lib,
   ...
 }: {
-  boot.loader.systemd-systemd-boot.enable = true;
+  boot.loader.systemd--boot.enable = true;
   # boot.loader.systemd-systemd-boot.enable = lib.mkForce false;
   # boot.loader.lanzaboote = {
   # enable = true;
