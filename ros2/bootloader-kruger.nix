24a29efdaa7d40fa5b3336c13db4b361265c76cb
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-10-08 15:57:26 +0200

    security theater for kruger

--- ros2/bootloader-kruger.nix
+++ ros2/bootloader-kruger.nix
@@ -3,12 +3,12 @@
   lib,
   ...
 }: {
-  boot.loader.systemd-boot.enable = true;
-  # boot.loader.systemd-systemd-boot.enable = lib.mkForce false;
-  # boot.loader.lanzaboote = {
-  # enable = true;
-  # pkiBundle = "/var/lib/sbctl";
-  # };
+  # boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-systemd-boot.enable = lib.mkForce false;
+  boot.loader.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
 
   environment.systemPackages = with pkgs; [sbctl];
 
