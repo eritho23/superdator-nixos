e57b56bb0d6f2269944823df8c48757fc6f165ad
Gustav Pettersson <gustav.pettersson2@outlook.com>
2025-10-17 10:32:42 +0200

    locked down ssh with group

--- ros2/common/users.nix
+++ ros2/common/users.nix
@@ -28,14 +28,14 @@
   users.users."erre" = {
     description = "Eric (230S)";
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = ["ssh-access" "wheel"];
     hashedPasswordFile = config.sops.secrets."ros2/users/erre/hashed_password".path;
   };
 
   users.users."gustav" = {
     description = "Gustav (240S)";
     isNormalUser = true;
-    extraGroups = ["wheel" "micromamba"];
+    extraGroups = ["ssh-access" "wheel" "micromamba"];
     hashedPasswordFile = config.sops.secrets."ros2/users/gustav/hashed_password".path;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVQjtd/jEPI3IgWyKiwvBD9S2hbLEZ249tOy8HpN2Ci gustav.pettersson2@outlook.com"
@@ -45,7 +45,7 @@
   users.users."jonathan" = {
     description = "Jonathan (240S)";
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = ["ssh-access" "wheel"];
     hashedPasswordFile = config.sops.secrets."ros2/users/jonathan/hashed_password".path;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbqVTascQiJYBzBLwvQhI8z/4bQh4NV9wNGkRicGnTd jonathan@wahrenberg.com"
