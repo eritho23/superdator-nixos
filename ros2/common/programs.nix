0206fc3ee73727757234b1b52a98e30ee71a7ac6
Gustav Pettersson <gustav.pettersson2@outlook.com>
2025-10-17 09:29:09 +0200

    cleanup

--- ros2/common/programs.nix
+++ ros2/common/programs.nix
@@ -10,83 +10,7 @@
     ];
   python312Env = pkgs.python312.withPackages python312Packages;
 
-  # FHS environment for isaac sim
-  # NOTE: we use isaac sim 5.0.0 as we have an rtx 50XX
-  isaacSimEnv = pkgs.buildFHSEnv {
-    name = "isaac-sim-env";
-    targetPkgs = pkgs:
-      with pkgs; [
-        bash
-        micromamba
-        python311
-
-        # Build tools (required for Isaac Lab and unitree_sdk2_python)
-        cmake
-        gcc
-        gnumake
-
-        # Basic utilities
-        git
-        which
-        zlib
-
-        # Libraries for Isaac Sim GUI/rendering
-        libGL
-        libGLU
-        xorg.libX11
-        xorg.libXext
-        xorg.libXrender
-
-        # For unitree_sdk2_python (cyclonedds dependency)
-        pkg-config
-        openssl
-
-        # Standard C++ library
-        stdenv.cc.cc.lib
-
-        # Vulkan /lib
-        xorg.libXi
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXinerama
-        libxkbcommon
-        vulkan-loader
-        alsa-lib
-        pulseaudio
-      ];
-    profile = ''
-      export SHELL=${pkgs.bash}/bin/bash
-      export MAMBA_ROOT_PREFIX=/opt/micromamba
-      export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
-
-      # Initialize micromamba env
-      export MICROMAMBA_EXE=${pkgs.micromamba}/bin/micromamba
-      eval "$($MICROMAMBA_EXE shell hook -s bash)"
-
-      # Welcome msg
-      clear
-      echo -e "\e[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
-      echo -e "\e[1;32m Welcome to the Isaac Sim FHS Environment!\e[0m"
-      echo -e "\e[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
-      echo -e "Micromamba root: \e[1;33m$MAMBA_ROOT_PREFIX\e[0m"
-      echo -e "Python version:  \e[1;33m$(python3 --version 2>/dev/null)\e[0m"
-      echo -e "Working dir:     \e[1;33m$(pwd)\e[0m"
-      echo
-
-      # Auto init micromamba env
-      AUTO_ENV="unitree_sim_env"
-      if [ -d "$MAMBA_ROOT_PREFIX/envs/$AUTO_ENV" ]; then
-        if micromamba activate "$AUTO_ENV" >/dev/null 2>&1; then
-          echo -e "\e[1;32mSuccessfully activated '$AUTO_ENV'\e[0m"
-        else
-          echo -e "\e[1;31mFailed to activate '$AUTO_ENV'\e[0m"
-        fi
-      else
-        echo -e "\e[1;33mUnable to find '$AUTO_ENV' this should not happen\e[0m"
-      fi
-    '';
-    runScript = "${pkgs.bash}/bin/bash -l -i"; # interactive login
-  };
+  isaac = import ./isaac-sim.nix {inherit pkgs;};
 in {
   nixpkgs.config.allowUnfree = true;
 
@@ -140,7 +64,7 @@ in {
 
     # custom env
     python312Env
-    isaacSimEnv
+    isaac.isaacSimEnv
   ];
 
   virtualisation.podman = {
