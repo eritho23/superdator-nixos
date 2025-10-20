1a758662521b01c6f969bc9c3a8677f75efc5bf5
Gustav Pettersson <gustav.pettersson2@outlook.com>
2025-10-20 15:57:58 +0200

    uefi

--- ros2/bootloader-leonardo.nix
+++ ros2/bootloader-leonardo.nix
@@ -1,12 +1,7 @@
-{ ... }: {
-  # Use GRUB for legacy (BIOS) boot
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda"; # replace with your actual disk (e.g. /dev/nvme0n1)
-    configurationLimit = 10;
-  };
+{...}: {
+  # simple
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  # Disable EFI/systemd-boot
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.systemd-boot.configurationLimit = 10;
 }
