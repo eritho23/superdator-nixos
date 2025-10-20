a9f6c91276ea13591cfaeef858166e7c224dad57
Gustav Pettersson <gustav.pettersson2@outlook.com>
2025-10-20 15:40:03 +0200

    donatello

---
+++ ros2/disko-donatello.nix
@@ -0,0 +1,45 @@
+{
+  disko.devices = {
+    disk.main = {
+      device = "/dev/disk/by-id/ata-ST500LT012-1DG142_S3P6D31Y";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "2G";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = ["umask=0077"];
+            };
+          };
+          root = {
+            end = "-32G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = ["noatime" "discard"];
+              extraArgs = [
+                "-L"
+                "nixos-root"
+                "-m"
+                "0"
+              ];
+            };
+          };
+          swap = {
+            size = "32G";
+            content = {
+              type = "swap";
+              priority = 100;
+            };
+          };
+        };
+      };
+    };
+  };
+}
