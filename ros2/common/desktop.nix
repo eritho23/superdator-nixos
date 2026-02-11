e572e42a7823a31f24b28f0d3beb2a955dcda223
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2026-02-11 08:53:38 +0100

    fix eval errs

--- ros2/common/desktop.nix
+++ ros2/common/desktop.nix
@@ -15,8 +15,8 @@
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
 
-  services.logind.extraConfig = ''
-    HandleHibernateKey=ignore
-    HandleSuspendKey=ignore
-  '';
+  services.logind.settings.Login = {
+    HandleHibernateKey = "ignore";
+    HandleSuspendKey = "ignore";
+  };
 }
