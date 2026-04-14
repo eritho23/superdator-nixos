bc99f71c5daaebe2564bd3c1c83730cdfa98fab8
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2026-04-14 12:35:33 +0200

    add erre keyä

--- nixos/deployments.nix
+++ nixos/deployments.nix
@@ -221,7 +221,10 @@ in {
           ];
         };
 
-        users.users."root".openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVQjtd/jEPI3IgWyKiwvBD9S2hbLEZ249tOy8HpN2Ci gustav.pettersson2@outlook.com"];
+        users.users."root".openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVQjtd/jEPI3IgWyKiwvBD9S2hbLEZ249tOy8HpN2Ci gustav.pettersson2@outlook.com"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5Gyc3DfKNsJ9UrF2xXuvsHe1BkvlltxsfUCYLniiqm abbindgym\23eritho@SW2311"
+        ];
         services.openssh.enable = true;
         services.openssh.openFirewall = true;
 
