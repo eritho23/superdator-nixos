1a6b67b9238a61cfccc20ec3398c008a26883d2e
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2026-05-08 12:29:55 +0200

    make csp managed by sveltekit

--- nixos/reverse-proxy.nix
+++ nixos/reverse-proxy.nix
@@ -115,7 +115,6 @@
           	X-Content-Type-Options "nosniff"
           	Referrer-Policy "no-referrer"
           	Permissions-Policy "geolocation=(), camera=(), microphone=()"
-          	Content-Security-Policy "default-src 'none'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://*.dzcdn.net; media-src 'self'; font-src 'self'; connect-src 'self'; form-action 'self'; base-uri 'self'; frame-ancestors 'self'; object-src 'none'; manifest-src 'self'"
           }
 
           @backend path /auth* /me* /spotify* /health* /rooms*
